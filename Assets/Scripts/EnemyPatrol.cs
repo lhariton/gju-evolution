349751b709593a68e67df9b3d3313b19630d6204
mdlna <mada.vasile99@gmail.com>
2024-03-10 00:28:12 +0200

    Add more animations for boss 2 and enemy

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -14,32 +14,41 @@ public class EnemyPatrol : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        currentPoint = pointB.transform;
+        currentPoint = pointA.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 point = currentPoint.position - transform.position;
-        if(currentPoint ==pointB.transform)
+        if(currentPoint ==pointA.transform)
         {
-            rb.velocity = new Vector2(speed, 0);
+            rb.velocity = new Vector2(-speed, 0);
         }
         else
         {
-            rb.velocity = new Vector2(-speed, 0);
+            rb.velocity = new Vector2(speed, 0);
         }
 
         Debug.Log(Vector2.Distance(transform.position, currentPoint.position));
         if(Vector2.Distance(transform.position, currentPoint.position)<0.5f && currentPoint == pointB.transform)
         {
+            flip();
             currentPoint = pointA.transform;
         }
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
         {
+            flip();
             currentPoint = pointB.transform;
         }
 
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
