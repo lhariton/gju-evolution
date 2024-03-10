931502f86145eefb95661677b2257d56b9fd40ca
Leonard Hariton <leohariton@gmail.com>
2024-03-10 10:31:09 +0200

    added red flash on boss hitting

--- Assets/Scripts/BossHealth.cs
+++ Assets/Scripts/BossHealth.cs
@@ -10,10 +10,8 @@ public class BossHealth : MonoBehaviour
     // Start is called before the first frame update
     public float health = 3;
     public Animator animatorPlayer;
-    void Start()
-    {
-        
-    }
+
+    public SpriteRenderer spriteRenderer;
 
     // Update is called once per frame
     void Update()
@@ -25,25 +23,25 @@ public class BossHealth : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    public IEnumerator flashRed()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.color = Color.white;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            //StartCoroutine(playerMovement.Knockback(0.03f, 350, playerMovement.transform.position));
+            // StartCoroutine(playerMovement.Knockback(0.03f, 350, playerMovement.transform.position));
+            StartCoroutine(flashRed());
             Debug.Log("Collision with player detected!");
             SoundFXManager.instance.PlaySoundFXClip(dmgAudioClip, transform, 1f);
             health -= 1;
             Debug.Log("Health: " + health);
         }
     }
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.CompareTag("Player"))
-    //    {
-    //        // Call your function or perform actions
-    //        Debug.Log("Collision with player detected!");
-    //        Debug.Log("Health: "+health);
-    //        health -= 1;
-    //    }
-    //}
+
 }
